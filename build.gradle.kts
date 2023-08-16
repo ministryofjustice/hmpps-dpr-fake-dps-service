9666591aec44c13060d23cd4c3b92889ebb9dbed
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-08-16 15:09:39 +0100

    [DPR-636] Build fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 }
 
