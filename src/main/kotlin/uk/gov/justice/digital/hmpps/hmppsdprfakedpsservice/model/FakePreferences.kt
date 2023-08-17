11e51837987c2860be86e29a65095d0de0ccbd88
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-08-17 16:07:58 +0100

    [DPR-636] Test fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsdprfakedpsservice/model/FakePreferences.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsdprfakedpsservice/model/FakePreferences.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 @Entity
 @Table(name = "fake_preferences")
@@ -20,4 +21,26 @@ data class FakePreferences(
   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
   val lastUpdated: LocalDateTime,
 
-)
+) {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as FakePreferences
+
+    if (prisonerNumber != other.prisonerNumber) return false
+    if (favouriteAnimal != other.favouriteAnimal) return false
+    if (favouriteColour != other.favouriteColour) return false
+
+    // Millisecond precision is truncated by Spring's mapping.
+    return lastUpdated.truncatedTo(ChronoUnit.SECONDS) == other.lastUpdated.truncatedTo(ChronoUnit.SECONDS)
+  }
+
+  override fun hashCode(): Int {
+    var result = prisonerNumber.hashCode()
+    result = 31 * result + favouriteAnimal.hashCode()
+    result = 31 * result + favouriteColour.hashCode()
+    result = 31 * result + lastUpdated.hashCode()
+    return result
+  }
+}
