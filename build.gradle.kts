3317a6599fb2ae1503f86658186c7d57d1525fca
Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>
2023-11-09 11:05:51 +0000

    [DPR2-122] Update library version for fix (#14)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
   // DPR Library
-  implementation("uk.gov.justice.service.hmpps:hmpps-digital-prison-reporting-lib:1.0.7")
+  implementation("uk.gov.justice.service.hmpps:hmpps-digital-prison-reporting-lib:1.0.9")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
   // Database
