d6db8b645c21bfcb1c832a7efa0cb1e30db72a73
Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>
2023-12-18 16:17:35 +0000

    [DPR2-000] Update library version (#34)
    * [DPR2-000] Update library version
    
    * [DPR2-000] Update library version
    
    * [DPR2-000] Banner

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsdprfakedpsservice/helper/FakeAuthAwareTokenConverter.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsdprfakedpsservice/helper/FakeAuthAwareTokenConverter.kt
@@ -3,19 +3,19 @@ package uk.gov.justice.digital.hmpps.hmppsdprfakedpsservice.helper
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.stereotype.Component
-import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.AuthAwareAuthenticationToken
-import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.AuthAwareTokenConverter
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.DprAuthAwareAuthenticationToken
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.DprAuthAwareTokenConverter
 import uk.gov.justice.digital.hmpps.hmppsdprfakedpsservice.security.FakeCaseloadProvider
 
 @Component
 class FakeAuthAwareTokenConverter(
   val fakeCaseloadProvider: FakeCaseloadProvider,
-) : AuthAwareTokenConverter {
+) : DprAuthAwareTokenConverter {
 
-  override fun convert(jwt: Jwt): AuthAwareAuthenticationToken {
+  override fun convert(jwt: Jwt): DprAuthAwareAuthenticationToken {
     val principal = "FAKE"
     val authorities = listOf(SimpleGrantedAuthority("ROLE_PRISONS_REPORTING_USER"))
 
-    return AuthAwareAuthenticationToken(jwt, principal, authorities, fakeCaseloadProvider)
+    return DprAuthAwareAuthenticationToken(jwt, principal, authorities, fakeCaseloadProvider)
   }
 }
