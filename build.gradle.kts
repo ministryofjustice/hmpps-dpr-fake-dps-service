9caee35868e75b028f70599ca50ae17c356bf51f
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2024-03-01 09:23:12 +0000

    [DPR2-175] Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
   // Database
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("org.postgresql:postgresql")
+  implementation("org.postgresql:postgresql:42.7.2")
 
   // Swagger
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
